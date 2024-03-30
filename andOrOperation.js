14327890e26dd9b91b997f9930ec557ebcf13f89
DhruvBhatt13 <bhattdhruv996@gmail.com>
2024-03-30 23:04:46 +0530

    and or operation

---
+++ andOrOperation.js
@@ -0,0 +1,11 @@
+// for or operation use ||
+var a=100,b=200,c=1000,d=40;
+if(a>=b && a>=c && a>=d){
+    console.log('a is greater');
+}else if(b>=c && b>=d){
+    console.log('b is greater');
+}else if(c>=d){
+    console.log('c is greater');
+}else{
+    console.log('d is greater');
+}
